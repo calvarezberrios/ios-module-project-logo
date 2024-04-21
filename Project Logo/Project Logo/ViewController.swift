c74c8c31081db88aa211f3b45ed1cb68ceb8f28b
Carlos "Mannie" Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2024-04-20 23:33:03 -0400

    Added .gitignore and created xcode project

---
+++ Project Logo/Project Logo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Logo
+//
+//  Created by Carlos E. Alvarez-Berrios on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
